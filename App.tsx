db18b18e69f063a177e15974ba0fd2c83892045f
Matheus <matheus.augustoreal@gmail.com>
2023-01-23 13:33:54 -0300

    feat: added OneSignal lib

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import OneSignal from "react-native-onesignal";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+const oneSignalAppId = process.env.ONE_SIGNAL_APP_ID
+  ? process.env.ONE_SIGNAL_APP_ID
+  : "config One Signal ID on .env file";
+
+OneSignal.setAppId(oneSignalAppId);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
